6d83b925f3820496de852b6784a86bd57d7f683b
ChauHuyDien <82036270+idiotman-2212@users.noreply.github.com>
2024-09-29 21:35:45 +0700

    Update script.sql

--- Database/script.sql
+++ Database/script.sql
@@ -1,3 +1,4 @@
+-- Bảng user
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `username` varchar(255) NOT NULL,
@@ -10,14 +11,13 @@ CREATE TABLE `user` (
   `last_name` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
+  `loyalty_points` decimal(10, 2) DEFAULT 0.00, -- Cột tích xu
   PRIMARY KEY (`username`),
-  UNIQUE KEY `UK_ob8kqyqqgmefl0aco34akdtpe` (`email`),
-  UNIQUE KEY `UK_589idila9li6a4arw1t8ht1gx` (`phone`)
+  UNIQUE KEY `UK_user_email` (`email`),
+  UNIQUE KEY `UK_user_phone` (`phone`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-LOCK TABLES `user` WRITE;
-UNLOCK TABLES;
 
-
+-- Bảng role
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
   `id` bigint NOT NULL AUTO_INCREMENT,
@@ -26,11 +26,8 @@ CREATE TABLE `role` (
   `role_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-LOCK TABLES `role` WRITE;
-INSERT INTO `role` VALUES (1,'Quản lý toàn hệ thống','ROLE_ADMIN','Quản trị viên'),(2,'Quản lý nghiệp vụ nhân viên','ROLE_STAFF','Nhân viên'),(3,'Khách hàng sử dụng dịch vụ','ROLE_CUSTOMER','Khách hàng'),(4,'Quyền tạo mới dữ liệu','ROLE_CREATE','Tạo mới'),(5,'Quyền xem dữ liệu','ROLE_READ','Đọc'),(6,'Quyền cập nhật dữ liệu','ROLE_UPDATE','Cập nhật'),(7,'Quyền xóa dữ liệu','ROLE_DELETE','Xóa');
-UNLOCK TABLES;
 
-
+-- Bảng token
 DROP TABLE IF EXISTS `token`;
 CREATE TABLE `token` (
   `id` bigint NOT NULL AUTO_INCREMENT,
@@ -40,14 +37,10 @@ CREATE TABLE `token` (
   `token_type` enum('BEARER') DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FKofh3vp2yx7uo49ane1s4dh6g3` (`username`),
-  CONSTRAINT `FKofh3vp2yx7uo49ane1s4dh6g3` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
+  CONSTRAINT `FK_token_user` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=84 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-LOCK TABLES `token` WRITE;
-UNLOCK TABLES;
 
-
-
+-- Bảng user_permission
 DROP TABLE IF EXISTS `user_permission`;
 CREATE TABLE `user_permission` (
   `id` bigint NOT NULL AUTO_INCREMENT,
@@ -55,16 +48,12 @@ CREATE TABLE `user_permission` (
   `role_id` bigint DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_user_role_screen` (`username`,`role_id`,`screen_code`),
-  KEY `FKhgr5k8iik7jdokqjdeg8nkwm5` (`role_id`),
-  CONSTRAINT `FK37krwnqotjma6n9hrjkdnj60n` FOREIGN KEY (`username`) REFERENCES `user` (`username`),
-  CONSTRAINT `FKhgr5k8iik7jdokqjdeg8nkwm5` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+  UNIQUE KEY `UK_user_permission` (`username`,`role_id`,`screen_code`),
+  CONSTRAINT `FK_user_permission_user` FOREIGN KEY (`username`) REFERENCES `user` (`username`),
+  CONSTRAINT `FK_user_permission_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-LOCK TABLES `user_permission` WRITE;
-UNLOCK TABLES;
 
-
-
+-- Bảng coach
 DROP TABLE IF EXISTS `coach`;
 CREATE TABLE `coach` (
   `id` bigint NOT NULL AUTO_INCREMENT,
@@ -73,14 +62,11 @@ CREATE TABLE `coach` (
   `license_plate` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_rgupf3gfwjyxhl148ua00f4un` (`license_plate`),
-  UNIQUE KEY `UK_sjv06n7tt5gkpndfwlaer0dkj` (`name`)
+  UNIQUE KEY `UK_coach_license_plate` (`license_plate`),
+  UNIQUE KEY `UK_coach_name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-LOCK TABLES `coach` WRITE;
-UNLOCK TABLES;
 
-
-
+-- Bảng discount
 DROP TABLE IF EXISTS `discount`;
 CREATE TABLE `discount` (
   `id` bigint NOT NULL AUTO_INCREMENT,
@@ -90,13 +76,10 @@ CREATE TABLE `discount` (
   `end_date_time` datetime(6) DEFAULT NULL,
   `start_date_time` datetime(6) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_i14w897ofrtv43vbx44rtv01u` (`code`)
+  UNIQUE KEY `UK_discount_code` (`code`)
 ) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-LOCK TABLES `discount` WRITE;
-UNLOCK TABLES;
 
-
-
+-- Bảng driver
 DROP TABLE IF EXISTS `driver`;
 CREATE TABLE `driver` (
   `id` bigint NOT NULL AUTO_INCREMENT,
@@ -110,16 +93,12 @@ CREATE TABLE `driver` (
   `phone` varchar(255) DEFAULT NULL,
   `quit` bit(1) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_fchuyotq64tagkwktlh4qttyy` (`email`),
-  UNIQUE KEY `UK_9yq25nknyh5y5gihylet1ypy9` (`license_number`),
-  UNIQUE KEY `UK_phx4fa0f4397mg8kltbf7c2gy` (`phone`)
+  UNIQUE KEY `UK_driver_email` (`email`),
+  UNIQUE KEY `UK_driver_license_number` (`license_number`),
+  UNIQUE KEY `UK_driver_phone` (`phone`)
 ) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-LOCK TABLES `driver` WRITE;
-UNLOCK TABLES;
-
-
-
+-- Bảng province
 DROP TABLE IF EXISTS `province`;
 CREATE TABLE `province` (
   `id` bigint NOT NULL AUTO_INCREMENT,
@@ -127,12 +106,8 @@ CREATE TABLE `province` (
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=127 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-LOCK TABLES `province` WRITE;
-UNLOCK TABLES;
 
-
-
-
+-- Bảng trip
 DROP TABLE IF EXISTS `trip`;
 CREATE TABLE `trip` (
   `id` bigint NOT NULL AUTO_INCREMENT,
@@ -145,22 +120,15 @@ CREATE TABLE `trip` (
   `driver_id` bigint DEFAULT NULL,
   `source_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_trip_fields` (`driver_id`,`coach_id`,`source_id`,`dest_id`,`departure_date_time`),
-  KEY `FKxq0qrdn0xmupsnq4mlplprke` (`coach_id`),
-  KEY `FK9qw6r3fpf34ldxj3febn4jiym` (`dest_id`),
-  KEY `FKs3c683ds4ckyessv3l8optnmb` (`discount_id`),
-  KEY `FK8ow8h4pjlljfdhlpbmh5lopyx` (`source_id`),
-  CONSTRAINT `FK8ow8h4pjlljfdhlpbmh5lopyx` FOREIGN KEY (`source_id`) REFERENCES `province` (`id`),
-  CONSTRAINT `FK9qw6r3fpf34ldxj3febn4jiym` FOREIGN KEY (`dest_id`) REFERENCES `province` (`id`),
-  CONSTRAINT `FKpuhkx68hnwy4by2b0onybv5x1` FOREIGN KEY (`driver_id`) REFERENCES `driver` (`id`),
-  CONSTRAINT `FKs3c683ds4ckyessv3l8optnmb` FOREIGN KEY (`discount_id`) REFERENCES `discount` (`id`),
-  CONSTRAINT `FKxq0qrdn0xmupsnq4mlplprke` FOREIGN KEY (`coach_id`) REFERENCES `coach` (`id`)
+  UNIQUE KEY `UK_trip` (`driver_id`,`coach_id`,`source_id`,`dest_id`,`departure_date_time`),
+  CONSTRAINT `FK_trip_coach` FOREIGN KEY (`coach_id`) REFERENCES `coach` (`id`),
+  CONSTRAINT `FK_trip_dest` FOREIGN KEY (`dest_id`) REFERENCES `province` (`id`),
+  CONSTRAINT `FK_trip_discount` FOREIGN KEY (`discount_id`) REFERENCES `discount` (`id`),
+  CONSTRAINT `FK_trip_driver` FOREIGN KEY (`driver_id`) REFERENCES `driver` (`id`),
+  CONSTRAINT `FK_trip_source` FOREIGN KEY (`source_id`) REFERENCES `province` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-LOCK TABLES `trip` WRITE;
-UNLOCK TABLES;
 
-
-
+-- Bảng booking
 DROP TABLE IF EXISTS `booking`;
 CREATE TABLE `booking` (
   `id` bigint NOT NULL AUTO_INCREMENT,
@@ -178,16 +146,14 @@ CREATE TABLE `booking` (
   `total_payment` decimal(38,2) DEFAULT NULL,
   `trip_id` bigint DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
+  `points_earned` decimal(10, 2) DEFAULT 0.00, -- Điểm tích lũy
+  `points_used` decimal(10, 2) DEFAULT 0.00, -- Điểm đã sử dụng
   PRIMARY KEY (`id`),
-  KEY `FKkp5ujmgvd2pmsehwpu2vyjkwb` (`trip_id`),
-  KEY `FKrjob56yal18kk1mvwj2d2elki` (`username`),
-  CONSTRAINT `FKkp5ujmgvd2pmsehwpu2vyjkwb` FOREIGN KEY (`trip_id`) REFERENCES `trip` (`id`),
-  CONSTRAINT `FKrjob56yal18kk1mvwj2d2elki` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
+  CONSTRAINT `FK_booking_trip` FOREIGN KEY (`trip_id`) REFERENCES `trip` (`id`),
+  CONSTRAINT `FK_booking_user` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-LOCK TABLES `booking` WRITE;
-UNLOCK TABLES;
 
-
+-- Bảng payment_history
 DROP TABLE IF EXISTS `payment_history`;
 CREATE TABLE `payment_history` (
   `id` bigint NOT NULL AUTO_INCREMENT,
@@ -196,27 +162,19 @@ CREATE TABLE `payment_history` (
   `status_change_date_time` datetime(6) DEFAULT NULL,
   `booking_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK78c8n1i1pc83oleh4xdbm46d5` (`booking_id`),
-  CONSTRAINT `FK78c8n1i1pc83oleh4xdbm46d5` FOREIGN KEY (`booking_id`) REFERENCES `booking` (`id`)
+  CONSTRAINT `FK_payment_history_booking` FOREIGN KEY (`booking_id`) REFERENCES `booking` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-LOCK TABLES `payment_history` WRITE;
-UNLOCK TABLES;
 
--- Tích xu để đổi thưởng-------------------------
-ALTER TABLE `user` ADD COLUMN `loyalty_points` DECIMAL(10, 2) DEFAULT 0.00;
-ALTER TABLE `booking` 
-ADD COLUMN `points_earned` DECIMAL(10, 2) DEFAULT 0.00,
-ADD COLUMN `points_used` DECIMAL(10, 2) DEFAULT 0.00;
+-- Bảng loyalty_transaction
+DROP TABLE IF EXISTS `loyalty_transaction`;
 CREATE TABLE `loyalty_transaction` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `amount` decimal(10, 2) NOT NULL,
   `transaction_date` datetime(6) NOT NULL,
   `transaction_type` enum('EARN', 'USE', 'EXPIRE') NOT NULL,
-  `booking_id` bigint,
+  `booking_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_loyalty_transaction_user` (`username`),
-  KEY `FK_loyalty_transaction_booking` (`booking_id`),
   CONSTRAINT `FK_loyalty_transaction_user` FOREIGN KEY (`username`) REFERENCES `user` (`username`),
   CONSTRAINT `FK_loyalty_transaction_booking` FOREIGN KEY (`booking_id`) REFERENCES `booking` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
